d077fee3301409c1c1b74ff2e96618935eb05c4b
Manish Singh <manishpurohit97@gmail.com>
2021-03-13 16:33:07 +0530

    basic program added

---
+++ First.java
@@ -0,0 +1,10 @@
+/**
+*Name: Manish Singh
+*Description: Demo of compilation & running of java program
+*Date: 3/12/2021
+**/
+class First {
+	public static void main(String[] args){
+		System.out.println("hello");
+	}
+}
